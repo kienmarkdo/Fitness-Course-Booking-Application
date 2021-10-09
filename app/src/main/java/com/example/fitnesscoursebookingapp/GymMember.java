3eb6f433b17034d198633623d72627f0ac88a3e5
Jordan Takefman <jordan.takefman@gmail.com>
2021-10-09 14:54:52 -0400

    Created GymMember.java.

---
+++ app/src/main/java/com/example/fitnesscoursebookingapp/GymMember.java
@@ -0,0 +1,17 @@
+package com.example.fitnesscoursebookingapp;
+
+public class GymMember {
+
+    private String name;
+
+    private String descriptionOfExperience;
+
+    Object[] coursesAttending = new Object[];
+
+    public Object[] courseAttending() {
+        return null;
+    }
+
+
+
+}
