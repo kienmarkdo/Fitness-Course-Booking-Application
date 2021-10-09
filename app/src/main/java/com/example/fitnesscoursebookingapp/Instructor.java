c82315ab1c66a44f8b82baa59e2150f3b8229180
Jordan Takefman <jordan.takefman@gmail.com>
2021-10-09 14:43:42 -0400

    Created Instructor.java (not extended to user yet couldn't pull)

---
+++ app/src/main/java/com/example/fitnesscoursebookingapp/Instructor.java
@@ -0,0 +1,36 @@
+package com.example.fitnesscoursebookingapp;
+
+public class Instructor {
+
+
+    private String name;
+    private String description;
+
+    private void selectCourseToTeach() {
+
+    }
+
+    private void setCourseDifficulty() {
+
+    }
+
+    private void setDateAndTime() {
+
+    }
+
+    private void setCapacity() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Course[] getCourseTeaching() {
+
+    }
+}
