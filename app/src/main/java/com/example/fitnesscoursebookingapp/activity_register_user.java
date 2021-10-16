a4c9a09570c8abaabd299d690c2f076c4d72fd6c
Kien Do <kienmarkdo@gmail.com>
2021-10-16 14:49:43 -0400

    Deleted redundant file

--- app/src/main/java/com/example/fitnesscoursebookingapp/activity_register_user.java
+++ app/src/main/java/com/example/fitnesscoursebookingapp/activity_register_user.java
@@ -5,17 +5,4 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class activity_register_user extends Activity {
-    public class activity_admin extends Activity {
-
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_admin);
-
-
-        }
-
-
-    }
 }
