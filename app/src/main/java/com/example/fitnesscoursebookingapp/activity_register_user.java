14ea9ae036b1fb7c9c9adde5eed736c761a6998f
Kien Do <kienmarkdo@gmail.com>
2021-10-18 18:22:29 -0400

    Added more comments

--- app/src/main/java/com/example/fitnesscoursebookingapp/activity_register_user.java
+++ app/src/main/java/com/example/fitnesscoursebookingapp/activity_register_user.java
@@ -22,6 +22,12 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+/**
+ * Register new account page of the Fitness Course Booking Application
+ *
+ * The user enters this page if they do not have a username and password yet and would like to
+ *  create one.
+ */
 public class activity_register_user extends Activity {
 
     // buttons and text fields
