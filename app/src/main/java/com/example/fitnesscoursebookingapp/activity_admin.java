16330b3a614cd1d3c1cfad501362e592f9b13736
Maximal03 <ikuru007@uottawa.ca>
2021-10-18 18:09:21 -0400

    Brainstorming for admin functionality

--- app/src/main/java/com/example/fitnesscoursebookingapp/activity_admin.java
+++ app/src/main/java/com/example/fitnesscoursebookingapp/activity_admin.java
@@ -4,22 +4,79 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class activity_admin extends Activity {
 
-    Button createClass,editClass, deleteClass;
+    Button createCourse,editCourse, deleteCourse;
+
+    //brainstorming
+    EditText courseNameInput;
+    EditText courseDescriptionInput;
+    EditText accountInput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 
+        //This code will have comments removed once the admin frontEnd has been implemented
+        //brainstorming
+        //
 
+        /*
+        createCourse = findViewById(R.id.createCourseButton);
+        editCourse = findViewById(R.id.editCourseButton);
+        deleteCourse = findViewById(R.id.deleteCourseButton);
+        deleteAccount = findViewById(R.id.deleteAccountButton);
+
+        courseNameInput = findViewById(R.id.courseNameInput); " for adding / deleting courses"
+        courseDescriptionInput = findViewById(R.id.courseDescriptionInput); "for adding a description"
+        accountInput = findViewById(R.id.accountInput); "for deleting a user account"
+
+          createCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { createCourse(); }
+        });
+
+          editCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { editCourse(); }
+        });
+
+          deleteCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { deleteCourse(); }
+        });
+
+          deleteAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { deleteAccount(); }
+        });
+
+         */
         TextView success = findViewById(R.id.loginSuccessfull);
-
 
     }
 
+    //commented out methods to add in once front end is complete
+    //may be separate depending on how front-end is implemented
+    private void createCourse(){
+
+    }
+
+    private void editCourse(){
+
+    }
+
+    private void deleteCourse(){
+
+    }
+
+    private void deleteAccount(){
+
+    }
 
 }
