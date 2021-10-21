30d668b9c36c84d9110d95570159b99a6d68f82e
Maximal03 <ikuru007@uottawa.ca>
2021-10-21 18:30:48 -0400

    Added variable names that were present in the UML diagram but not in the code

--- app/src/main/java/com/example/fitnesscoursebookingapp/Gym.java
+++ app/src/main/java/com/example/fitnesscoursebookingapp/Gym.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 public class Gym {
 
    // static for use between all activities
+   static int numberOfUsers;
    static HashMap<String, Instructor> listOfInstructors = new HashMap<String, Instructor>();
    static HashMap<String, GymMember> listOfGymMember = new HashMap<String, GymMember>();
    static HashMap<String, Course> listOfCourses = new HashMap<String, Course>();
