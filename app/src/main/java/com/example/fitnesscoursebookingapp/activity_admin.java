7abf6f9da8d7863d30dc76ee8f14716fe9095e17
Kien Do <kienmarkdo@gmail.com>
2021-10-20 21:05:04 -0400

    Create Course functionality PARTIALLY done (read description)
    Check the TODO. Setting on click listener to createCourseButton causes the app to crash after logging in as admin. Figure out why that is and fix it.

--- app/src/main/java/com/example/fitnesscoursebookingapp/activity_admin.java
+++ app/src/main/java/com/example/fitnesscoursebookingapp/activity_admin.java
@@ -3,28 +3,40 @@ package com.example.fitnesscoursebookingapp;
 import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class activity_admin extends Activity implements View.OnClickListener {
 
     RelativeLayout relativeLayout, relativeLayout1, relativeLayout2, relativeLayout3;
     Button viewmore, viewmore1, viewmore2, viewmore3;
-    //Button createCourseButton, editCourseButton, deleteCourseButton, deleteUserButton;
+    Button createCourseButton, editCourseButton, deleteCourseButton, deleteUserButton;
     int height, height1, height2, height3;
 
-    /*EditText addCourseInput;
+    EditText addCourseInput;
     EditText addCourseDescriptionInput;
     EditText courseNameChangeInput;
     EditText courseDescriptionChangeInput;
     EditText deleteCourseNameInput;
-    EditText deleteUserNameInput;*/
+    EditText deleteUsernameInput;
 
 
     @Override
@@ -49,21 +61,23 @@ public class activity_admin extends Activity implements View.OnClickListener {
         viewmore3.setOnClickListener(this);
 
         // initializers for different Admin Commands
-        /*addCourseInput = findViewById(R.id.addCourseInput);
+        addCourseInput = findViewById(R.id.addCourseInput);
         addCourseDescriptionInput = findViewById(R.id.addCourseDescriptionInput);
         courseNameChangeInput = findViewById(R.id.courseNameChangeInput);
         courseDescriptionChangeInput = findViewById(R.id.courseDescriptionChangeInput);
         deleteCourseNameInput = findViewById(R.id.deleteCourseNameInput);
-        deleteUserNameInput = findViewById(R.id.deleteUserNameInput);
+        deleteUsernameInput = findViewById(R.id.deleteUserNameInput);
 
+        // TODO: setting on click listener on createCourseButton will cause the app to crash after logging in
+        //  Commenting out this block of code below will stop the app from crashing.
         createCourseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 createCourse();
             }
-
         });
 
+        /*
         editCourseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -217,10 +231,57 @@ public class activity_admin extends Activity implements View.OnClickListener {
 
     // **** METHODS FOR ADMIN BUTTONS ****
 
-   /* private void createCourse(){
+    /**
+     * Creates a new course by taking in the name and description of the new course as user input
+     * Adds the course info into the firebase database.
+     */
+    private void createCourse() {
 
-    }
+        String courseNameStr = addCourseInput.getText().toString();
+        String courseDescriptionStr = addCourseDescriptionInput.getText().toString();
 
+        // =======  check if the course name is empty or not  =======
+        if (courseNameStr.equals("")) {
+            addCourseInput.setError("The course name cannot be blank.");
+            addCourseDescriptionInput.requestFocus();
+            return;
+        }
+
+        // NOTE: The input is case sensitive, which means the course "Tennis" and "tennis" may co-exist at the same time
+
+        // =======  check if the course exists in the database or not  =======
+
+        // fetches instance of database.
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Courses");
+
+        // Orders in search for the course name
+        Query checkCourse = reference.orderByChild("name").equalTo(courseNameStr);
+
+        checkCourse.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // checks whether the username already exists or not
+                if (dataSnapshot.exists()) {
+                    addCourseInput.setError("This username already exists. Please re-enter a new course name.");
+                    addCourseInput.requestFocus();
+                } else {
+                    addCourseInput.setError(null);
+                    Course newCourse = new Course(courseNameStr, courseDescriptionStr);
+                    reference.push().setValue(newCourse); // add new course here
+                    printCourseAddedSuccessMessage();
+                } // end of outer if/else
+
+            } // end of onDataChange()
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            } // end of onCalled()
+        }); // end of checkCourse listener
+
+    } // end of createCourse()
+
+    /*
     private void editCourse(){
 
     }
@@ -232,6 +293,32 @@ public class activity_admin extends Activity implements View.OnClickListener {
     private void deleteUser(){
 
     }*/
+
+
+    /**
+     * Displays a small pop-up at the bottom of the screen indicating that adding a course was a success
+     */
+    private void printCourseAddedSuccessMessage() {
+
+        // hides the keyboard
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+
+        addCourseInput.getText().clear();
+        addCourseDescriptionInput.getText().clear();
+
+        // displays the success message
+        Context context = getApplicationContext();
+        CharSequence text = "Course Added Successfully!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    } // end of printCourseAddedSuccessMessage()
+
 
 } // end of activity_admin
 
