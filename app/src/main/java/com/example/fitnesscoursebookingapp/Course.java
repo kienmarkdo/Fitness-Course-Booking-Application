133ea5f44538521b8d4ae05fcf30f919a9c0d5a6
philipostr <philipostroscki@yahoo.ca>
2021-10-20 20:33:55 -0400

    Removed redundant information from Course.java.

--- app/src/main/java/com/example/fitnesscoursebookingapp/Course.java
+++ app/src/main/java/com/example/fitnesscoursebookingapp/Course.java
@@ -12,10 +12,8 @@ public class Course {
     private String name;
     private String description;
     private String experienceLevel; // indicates the experience a student should have before enrolling (i.e.: beginner, expert...)
-    private String courseType; // name of the course type (i.e.: HIIT, Tennis, Badminton...)
     private String time; // hour, date, month, year
     private float hourDuration; // how long the course lasts (i.e.: 1.5 hours)
-    private int courseID; // a unique identifier for a course - useful for database organization
     private int numberOfStudents; // number of students enrolled in a course
     private GymMember[] students; // list of all students (this stays as an array because there is a max num of students)
 
@@ -23,6 +21,11 @@ public class Course {
     public Course() {
         // set size of the array GymMember here; that indicates the max amount of students a course can have
         numberOfStudents = 0;
+    }
+
+    public Course(String name, String description) {
+        this.name = name;
+        this.description = description;
     }
 
     /** Get and Set methods */
@@ -56,14 +59,6 @@ public class Course {
 
     public void setExperienceLevel(String experienceLevel) {
         this.experienceLevel = experienceLevel;
-    }
-
-    public String getCourseType() {
-        return courseType;
-    }
-
-    public void setCourseType(String courseType) {
-        this.courseType = courseType;
     }
 
     public String getTime() {
