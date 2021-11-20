542c04dbeeaa972775338554c02b90ebe7068087
Maximal03 <ikuru007@uottawa.ca>
2021-11-20 15:10:20 -0500

    Added the Junit testing class for the admin class. Can add tests as necessary

---
+++ app/src/test/java/com/example/fitnesscoursebookingapp/AdminJunit.java
@@ -0,0 +1,36 @@
+package com.example.fitnesscoursebookingapp;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class AdminJunit {
+
+    //general tests brainstorming. Can change as necessary
+
+    @Test
+    public void testCreateCourse(){
+
+
+    }
+
+    @Test
+    public void testDeleteCourse(){
+
+    }
+
+    @Test
+    public void testEditCourse(){
+
+    }
+
+    @Test
+    public void testCreateCourseNameEmpty(){
+
+    }
+
+    @Test
+    public void testDeleteUserIfExist(){
+
+    }
+}
